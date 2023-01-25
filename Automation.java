ab76ab9385a3d395ff649f96b0749b2d068e0160
JeevithaVishwanath <121374453+JeevithaVishwanath@users.noreply.github.com>
2023-01-25 18:34:54 +0530

    Add files via upload

---
+++ Automation.java
@@ -0,0 +1,31 @@
+package week4.day1;
+
+public class Automation extends MultipleLanguage implements Language,TestTool{
+
+	public void selenium() {
+		System.out.println("Selenium");
+		
+	}
+
+	public void java() {
+	System.out.println("Java");
+		
+	}
+
+	@Override
+	public void ruby() {
+		System.out.println("Ruby");
+		
+	}
+	public static void main(String[] args) {
+		
+		Automation executes=new Automation();
+		executes.selenium();
+		executes.java();
+		executes.ruby();
+		executes.python();
+	}
+	
+	
+
+}
---
+++ MultipleLanguage.java
@@ -0,0 +1,11 @@
+package week4.day1;
+
+public abstract class MultipleLanguage {
+
+	public void python() {
+		
+		System.out.println("Language Python");
+	}
+	
+	public abstract void ruby();
+}
